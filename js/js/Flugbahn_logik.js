5c32db5103ca17f7b5a4e01225baaa88d2e4d244
mlauenroth <Malte@Malte-PC>
2011-10-01 02:29:24 +0200

    wd

--- js/js/Flugbahn_logik.js
+++ js/js/Flugbahn_logik.js
@@ -2,6 +2,6 @@ schnitt: Function (r, s, rad) {
 var rs = r.dot(s)
 var rr = r.dot(r)
 var int = Math.sqrt(rs * rs - s.dot(s) * rr - rad)
-return t1 = (rs + int) / (rr -rad)
-return t2 = (rs - int) / (rr -rad)
+return r.scalar((rs + int) / (rr -rad))
+return r.scalar((rs - int) / (rr -rad))
 }
