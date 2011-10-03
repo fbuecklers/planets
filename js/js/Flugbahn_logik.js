45331bcf12d7bd14c96495e6eee7a89119cd6aa1
florian <fbuecklers@googlemail.com>
2011-10-04 01:40:20 +0200

    Change-Id: Ib2b1914909c6217a35b59b1af1d0acf403b10430

--- js/js/Flugbahn_logik.js
+++
@@ -1,7 +0,0 @@
-schnitt: Function (r, s, rad) {
-var rs = r.dot(s)
-var rr = r.dot(r)
-var int = Math.sqrt(rs * rs - s.dot(s) * rr - rad)
-return r.scalar((rs + int) / (rr -rad))
-return r.scalar((rs - int) / (rr -rad))
-}
